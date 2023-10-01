ca37fe2b6ba94b0890dbb9697f5541562f16a386
loganfalzarano <lfalzar1@jh.edu>
2023-10-01 15:43:57 -0400

    removing button

--- bet-blitz/src/pages/index.tsx
+++ bet-blitz/src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <button onClick={() => seedDb()}>Seed DB</button>
+            {/* <button onClick={() => seedDb()}>Seed DB</button> */}
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
