4ebf6a76b56720c5e7acbaffd1646813335f4ba3
loganfalzarano <lfalzar1@jh.edu>
2023-10-13 12:25:16 -0400

    pushing all changes for chirp feature

--- bet-blitz/src/pages/_app.tsx
+++ bet-blitz/src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { Toaster } from "~/components/ui/toaster"
+
 
 import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
@@ -10,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
+      <Toaster />
     </ClerkProvider>
   );
 }
