f57048fa2ae27d5016f1fd87a1b687293a3ba849
Shengran Guo <143652868+Shengran123@users.noreply.github.com>
2023-10-16 11:13:41 -0400

    second commit

--- bet-blitz/src/pages/search/index.tsx
+++ bet-blitz/src/pages/search/index.tsx
@@ -1,178 +1,194 @@
 import Head from "next/head";
-import { createClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
-import { Event } from "@prisma/client";
-import { Card, CardHeader, CardTitle } from "~/components/ui/card";
-import { Button } from "~/components/ui/button";
-import { Badge } from "~/components/ui/badge";
-import { UserButton } from "@clerk/nextjs";
-import { User } from "lucide-react";
-
-import { supabaseClient } from "~/utils/supabaseClient";
-import { useAuth } from "@clerk/nextjs";
-
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import {createClient} from "@supabase/supabase-js";
+import {useEffect, useState} from "react";
+import {Event} from "@prisma/client";
+import {Card, CardHeader, CardTitle} from "~/components/ui/card";
+import {Badge} from "~/components/ui/badge";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {User} from "lucide-react";
 
 // import '~/styles/search.css';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import {useRef} from "react";
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+
+function SearchForm({onSubmit}) {
+    const formRef = useRef(null);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+      
+        onSubmit({
+            homeTeam: formRef.current[0].value,
+            awayTeam: formRef.current[1].value,
+            teamOneName: formRef.current[2].value,
+            teamTwoName: formRef.current[3].value,
+            commenceTime: formRef.current[4].value,
+            createdAt: formRef.current[5].value,
+            updatedAt: formRef.current[6].value,
+        })
+
+    }
+    return (
+        <Form ref={formRef} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+                <Form.Group as={Col} controlId="homeTeam">
+                    <Form.Label>Home Team</Form.Label>
+                    <Form.Control/>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="awayTeam">
+                    <Form.Label>Away Team</Form.Label>
+                    <Form.Control/>
+                </Form.Group>
+
+            </Row>
+            <Row className="mb-3">
+                <Form.Group as={Col} controlId="teamOneName">
+                    <Form.Label>Team One Name</Form.Label>
+                    <Form.Control/>
+                </Form.Group>
+                <Form.Group as={Col} controlId="teamTwoName">
+                    <Form.Label>Team Two Name</Form.Label>
+                    <Form.Control/>
+                </Form.Group>
+            </Row>
+
+            {/*DateTime Picker*/}
+            <Row className="mb-3">
+                <Form.Group as={Col} controlId="commenceTime">
+                    <Form.Label>Commence Time</Form.Label>
+                    <Form.Control type="datetime-local"/>
+                </Form.Group>
+                <Form.Group as={Col} controlId="createdAt">
+                    <Form.Label>Created At</Form.Label>
+                    <Form.Control type="datetime-local"/>
+                </Form.Group>
+                <Form.Group as={Col} controlId="updatedAt">
+                    <Form.Label>UpdatedAt</Form.Label>
+                    <Form.Control type="datetime-local"/>
+                </Form.Group>
+            </Row>
+
+            {/*clear button*/}
+            <Button className="mr-3" onClick={() => formRef.current.reset()}>
+                Clear
+            </Button>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
+}
 
 
 const supabaseUrl = "http://localhost:54321";
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0");
 export default function Search() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [searchResults, setSearchResults] = useState([]);
-  const [events, setEvents] = useState<Event[]>([]);
-  const { userId, getToken } = useAuth();
-  const [list,setList]=useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [searchResults, setSearchResults] = useState([]);
+    const [events, setEvents] = useState<Event[]>([]);
 
-  const fetchData = async () => {
-    if (!selectedDate) return;
-    console.log(selectedDate, searchQuery);
-    const currentSelectedDate = `${selectedDate.getFullYear()}-${(selectedDate.getMonth() + 1).toString().padStart(2, "0")}-${selectedDate.getDate().toString().padStart(2, "0")}`;
-    const timestamp = selectedDate.getTime();
-    const { data, error } = await supabase
-      .from("Event")
-      .select("*")
-      .or(`teamOneName.ilike.%${searchQuery}%,teamTwoName.ilike.%${searchQuery}%`)
-      .gte("createdAt", currentSelectedDate);
-    //.gte("createdAt", timestamp)
+    const [list, setList] = useState([]);
 
-    console.log(data, error, "database");
-    setList(data)
-  };
+    /*
+        const fetchData = async () => {
+            if (!selectedDate) return;
+            console.log(selectedDate, searchQuery);
+            const currentSelectedDate = `${selectedDate.getFullYear()}-${(selectedDate.getMonth() + 1).toString().padStart(2, "0")}-${selectedDate.getDate().toString().padStart(2, "0")}`;
+            const timestamp = selectedDate.getTime();
+            const {data, error} = await supabase
+                .from("Event")
+                .select("*")
+                .or(`teamOneName.ilike.%${searchQuery}%,teamTwoName.ilike.%${searchQuery}%`)
+                .gte("createdAt", currentSelectedDate);
+            //.gte("createdAt", timestamp)
 
+            console.log(data, error, "database");
+            setList(data)
+        };
+    */
+    const handleQuery =async (data) => {
 
-  return (
-    <>
-      <Head>
-        <title>Search</title>
-        <meta name="description" content="Generated by create-t3-app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="container">
-        <h1 className="text-red-400">Search Page</h1>
-        <div className="filters">
-          <input
-            type="text"
-            placeholder="Search..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="searchInput"
-          />
-          <DatePicker
-            selected={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
-            showTimeSelect
-            timeFormat="HH:mm"
-            dateFormat="yyyy/MM/dd HH:mm"
-            className="datePicker"
-          />
-          <Button onClick={() => fetchData()} className="searchButton">
-            Search
-          </Button>
-        </div>
+        const queryConnector = []
+        Object.keys(data).forEach(fieldName => {
+            switch (fieldName) {
+                case "homeTeam":
+                case "awayTeam":
+                case "teamOneName":
+                case "teamTwoName":
+                    if (data[fieldName]) {
+                        queryConnector.push(`${fieldName}.ilike.%${data[fieldName]}%`)
+                    }
+                    break;
+                case "commenceTime":
+                case "createdAt":
+                case "updatedAt":
+                    if (data[fieldName]) {
+                        queryConnector.push(`${fieldName}.gte.${getDateTimeFormat(new Date(data[fieldName]))}`)
+                    }
+                    break
 
-        <table>
-          <thead>
-          <tr>
-            <td>homeTeam</td>
-            <td>awayTeam</td>
-            <td>teamOneName</td>
+            }
+        })
+        console.log(queryConnector)
+        const {data: resp, error} = await supabase.from("Event").select("*").or(queryConnector.join(","));
+        setList(resp)
 
-            <td>createdAt</td>
-            {/* Add more table headers as needed */}
-          </tr>
-          </thead>
-          <tbody>
-          {list.map((item) => (
-            <tr key={item.id}>
-              <td>{item.homeTeam}</td>
-              <td>{item.awayTeam}</td>
-              <td>{item.teamOneName}</td>
-              <td>{item.createdAt}</td>
+    }
 
-              {/* <td>{dateToString(event.commenceTime ? event.commenceTime : new Date())}</td> */}
-              {/* Add more table data cells as needed */}
-            </tr>
-          ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+    const getDateTimeFormat = (date) => {
+        const currentSelectedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+        console.log(currentSelectedDate)
+        return currentSelectedDate;
+    }
+    return <>
+            <Head>
+                <title>Search</title>
+                <meta name="description" content="Generated by create-t3-app"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <Container>
+                <h1>Search Page</h1>
+                <Row className="mb-3">
+                    <SearchForm onSubmit={handleQuery}/>
+                </Row>
+                <Row>
 
+                </Row>
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>homeTeam</th>
+                        <th>awayTeam</th>
+                        <th>teamOneName</th>
 
-  // const [searchText, setSearchText] = useState('');
-  // const [filterTime, setFilterTime] = useState('');
-  // const [results, setResults] = useState([]);
-  // const [events, setEvents] = useState<Event[]>([]);
-  // const { userId, getToken } = useAuth();
+                        <th>createdAt</th>
+                        {/* Add more table headers as needed */}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {list.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.homeTeam}</td>
+                            <td>{item.awayTeam}</td>
+                            <td>{item.teamOneName}</td>
+                            <td>{item.createdAt}</td>
 
-  // const handleSearch = async () => {
-  //   const token = await getToken({template: "supabase"});
-  //   const supabase = await supabaseClient(token);
-  //   const { data, error } = await supabase
-  //     .from('Event')
-  //     .select()
-  //     .ilike('homeTeam', `%${searchText}%`)
-  //     .or('awayTeam', 'ilike', `%${searchText}%`)
-  //     .or('teamOneName', 'ilike', `%${searchText}%`)
-  //     .eq('commerceTime', filterTime);
-
-  //   if (error) {
-  //     console.error('Error fetching data:', error);
-  //   } else {
-  //     setResults(data);
-  //   }
-  // };
-
-  // return (
-  //   <div>
-  //     <h1>Commerce Search</h1>
-  //     <div>
-  //       <input
-  //         type="text"
-  //         placeholder="Search by keyword"
-  //         onChange={(e) => setSearchText(e.target.value)}
-  //         style={{ padding: '5px', width: '200px' }}
-  //       />
-  //       <DatePicker
-  //         selected={filterTime}
-  //         onChange={(date) => setFilterTime(date)}
-  //         showTimeSelect
-  //         dateFormat="yyyy-MM-dd HH:mm:ss"
-  //         style={{ padding: '5px' }}
-  //       />
-  //       <button onClick={handleSearch} style={{ padding: '5px' }}>
-  //         Search
-  //       </button>
-  //     </div>
-  //     <ResultsTable>
-  //       <thead>
-  //         <tr>
-  //           <th>homeTeam</th>
-  //           <th>awayTeam</th>
-  //           <th>teamOneName</th>
-  //           {/* Add more table headers as needed */}
-  //         </tr>
-  //       </thead>
-  //       <tbody>
-  //         {results.map((result, index) => (
-  //           <tr key={index}>
-  //             <td>{result.homeTeam}</td>
-  //             <td>{result.awayTeam}</td>
-  //             <td>{result.teamOneName}</td>
-  //             {/* Add more table data cells as needed */}
-  //           </tr>
-  //         ))}
-  //       </tbody>
-  //     </ResultsTable>
-  //   </div>
-  // );
-};
-
-
-
+                            {/* <td>{dateToString(event.commenceTime ? event.commenceTime : new Date())}</td> */}
+                            {/* Add more table data cells as needed */}
+                        </tr>
+                    ))}
+                    </tbody>
+                </Table>
+           </Container>
+        </>
+  
+}
