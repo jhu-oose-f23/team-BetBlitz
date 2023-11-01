7ffe582dbd8890dc95245cc1882e622c753d873a
loganfalzarano <lfalzar1@jh.edu>
2023-11-01 11:32:53 -0400

    league form

--- bet-blitz/src/pages/league/index.tsx
+++ bet-blitz/src/pages/league/index.tsx
@@ -3,9 +3,9 @@ import LeagueForm from "~/components/league/LeagueForm"
 
 export default function League() {
     return (
-        <div className="flex items-center justify-center h-4/5">
-            {/* <LeagueForm /> */}
-            <PercentageCard></PercentageCard>
+        <div className=" w-full">
+            <LeagueForm />
+            {/* <PercentageCard></PercentageCard> */}
         </div>
     )
 }
