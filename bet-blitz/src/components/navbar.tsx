92db2b0daba52ce061c32c797aa3d6c5f14da651
loganfalzarano <lfalzar1@jh.edu>
2023-11-15 17:56:21 -0500

    small navbar fix

--- bet-blitz/src/components/navbar.tsx
+++ bet-blitz/src/components/navbar.tsx
@@ -78,7 +78,7 @@ export function Navbar() {
                   href="/parlay"
                   className={twMerge(
                     "rounded-md px-3 py-2 text-sm font-medium text-gray-100 hover:bg-gray-700 hover:text-white",
-                    router.pathname === "/league" ? "bg-gray-500" : "",
+                    router.pathname === "/parlay" ? "bg-gray-500" : "",
                   )}
                 >
                   Parlay
