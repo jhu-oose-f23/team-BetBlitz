f2da4e97914480cb337f1a2923fe0ed684a766dc
loganfalzarano <lfalzar1@jh.edu>
2023-12-01 13:51:18 -0500

    refactoring analytics to remove duplicate code and api calls

--- bet-blitz/src/components/analytics/BetsCard.tsx
+++ bet-blitz/src/components/analytics/BetsCard.tsx
@@ -2,12 +2,23 @@ import { Bet } from "@prisma/client";
 import AnalyticsCard from "./AnalyticsCard";
 import BetsTable from "./BetsTable";
 
-const BettingPercentageCard = () => {
+import { BetWithEvent } from "~/pages/analytics";
+
+type PropType = {
+  bets: BetWithEvent[];
+};
+
+//take bets in as a prop
+
+
+const BettingPercentageCard = (props: PropType) => {
+  const { bets } = props;
+
   return (
     <div>
       <AnalyticsCard
         title={"Recent Bets"}
-        component={<BetsTable />}
+        component={<BetsTable bets={bets} />}
       ></AnalyticsCard>
     </div>
   );
